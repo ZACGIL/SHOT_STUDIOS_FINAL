f5cbad291ed3fd6d0edf9831623a3b5cd9d500fe
vinceyangeles <30303455+vinceyangeles@users.noreply.github.com>
2018-04-09 11:09:42 +1000

    Create C++ AI

---
+++ SHOT_STUDIOS_FINAL-master/SHOT_STUDIOS_FINAL-master/SHOT_STUDIOS_MASTER_Copy/Intermediate/Build/Win64/UE4Editor/Inc/SHOT_STUDIOS_MASTER/AIPatrol.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SHOT_STUDIOS_MASTER_AIPatrol_generated_h
+#error "AIPatrol.generated.h already included, missing '#pragma once' in AIPatrol.h"
+#endif
+#define SHOT_STUDIOS_MASTER_AIPatrol_generated_h
+
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_RPC_WRAPPERS
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAIPatrol(); \
+	friend SHOT_STUDIOS_MASTER_API class UClass* Z_Construct_UClass_AAIPatrol(); \
+public: \
+	DECLARE_CLASS(AAIPatrol, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/SHOT_STUDIOS_MASTER"), NO_API) \
+	DECLARE_SERIALIZER(AAIPatrol) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAAIPatrol(); \
+	friend SHOT_STUDIOS_MASTER_API class UClass* Z_Construct_UClass_AAIPatrol(); \
+public: \
+	DECLARE_CLASS(AAIPatrol, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/SHOT_STUDIOS_MASTER"), NO_API) \
+	DECLARE_SERIALIZER(AAIPatrol) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAIPatrol(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAIPatrol) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAIPatrol); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAIPatrol); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAIPatrol(AAIPatrol&&); \
+	NO_API AAIPatrol(const AAIPatrol&); \
+public:
+
+
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAIPatrol(AAIPatrol&&); \
+	NO_API AAIPatrol(const AAIPatrol&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAIPatrol); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAIPatrol); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AAIPatrol)
+
+
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_PRIVATE_PROPERTY_OFFSET
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_9_PROLOG
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_PRIVATE_PROPERTY_OFFSET \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_RPC_WRAPPERS \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_INCLASS \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_PRIVATE_PROPERTY_OFFSET \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_INCLASS_NO_PURE_DECLS \
+	SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SHOT_STUDIOS_MASTER_Copy_Source_SHOT_STUDIOS_MASTER_AIPatrol_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
